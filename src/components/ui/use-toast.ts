cd6238191bde409f5a58e99f6352a3b16f3e8834
Aniket Jagtap <jagtapaniket731712@gmail.com>
2026-08-31 09:39:51 +0530

    some fixes

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -3,3 +3,5 @@ import { useToast, toast } from "@/hooks/use-toast";
 export { useToast, toast };
 
 // Project by Aniket Jagtap, time: - 2026-08-28 22:49:04
+
+// Project by Aniket Jagtap, time: - 2026-08-31 09:39:51
