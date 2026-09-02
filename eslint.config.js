74b6488850b28871db6966a7af0e08001018c407
Aniket Jagtap <jagtapaniket731712@gmail.com>
2026-09-02 11:01:43 +0530

    some fixes

--- eslint.config.js
+++ eslint.config.js
@@ -28,3 +28,5 @@ export default tseslint.config(
 // Project by Aniket Jagtap, time: - 2026-08-30 03:17:34
 
 // Project by Aniket Jagtap, time: - 2026-09-01 03:01:52
+
+// Project by Aniket Jagtap, time: - 2026-09-02 11:01:44
