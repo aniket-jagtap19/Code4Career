87690d3fcdfd163b5c416e8b3dfea88e983825e1
Aniket Jagtap <jagtapaniket731712@gmail.com>
2026-09-03 09:48:25 +0530

    some fixes

--- postcss.config.js
+++ postcss.config.js
@@ -8,3 +8,5 @@ export default {
 // Automated maintenance update - 2026-08-28 22:40:48
 
 // Project by Aniket Jagtap, time: - 2026-08-29 11:49:39
+
+// Project by Aniket Jagtap, time: - 2026-09-03 09:48:27
