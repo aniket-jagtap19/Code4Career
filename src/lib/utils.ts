7dd4615d005bcddc0e02908a4a008a1e39c9d13d
Aniket Jagtap <jagtapaniket731712@gmail.com>
2026-09-06 22:40:15 +0530

    some fixes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// Project by Aniket Jagtap, time: - 2026-09-06 22:40:21
